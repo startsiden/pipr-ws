64420cf51af7fb0cdd63c1add7302ee7df85ba88
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2014-03-25 13:48:59 +0100

    RM-8242: Bump version for release 14.13.8

--- lib/Pipr/WS.pm
+++ lib/Pipr/WS.pm
@@ -25,7 +25,7 @@ use Cwd;
 use URI;
 use URI::Escape;
 
-our $VERSION = '14.13.7';
+our $VERSION = '14.13.8';
 
 my $ua = LWPx::ParanoidAgent->new();
 $ua->whitelisted_hosts( @{ config->{whitelisted_hosts} } );
